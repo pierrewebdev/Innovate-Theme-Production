40c06575cef161ab52c967426db4bf32cc7ad25d
Patrick Pierre <patrick.pierre000@gmail.com>
2022-09-27 01:50:11 -0400

    Added new gulp-babel package to project to allow me to convert ES6 Javascript to ES5 which is the version of JavaScript that Shopify accepts. The only thing I need to do now is organize the files that Shopify needs as a dist folder so that I can serve the files exactlly as Shopify expects it

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 //run gulp
 
 var gulp = require('gulp');
+var babel = require('gulp-babel')
 var sass = require('gulp-sass');
 var replace = require('gulp-replace');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');	
 
+// =========================================================  //
+//Next two Gulp Tasks have to do with converting Sass to CSS
 gulp.task('sass', function() {
 //root scss file (import all your partials into here)
 return gulp.src('./sass/styles.scss')
@@ -26,4 +29,19 @@ gulp.task('default', function() {
     // this assumes your sass is in a directory named sass
     gulp.watch(['./sass/**/*.scss'], gulp.series('sass'));
 });
+
+// =========================================================  //
+
+//Next 2 tasks have to do with compiling javascript from ES6 to ES5 with babel
+var requiredFiles = ['javascript/**/*.js',]
+
+gulp.task('babelTest', async function () {
+    return gulp.src(requiredFiles)
+        .pipe(babel({presets: ['@babel/preset-env'] })) 
+        .pipe(gulp.dest('./assets'));
+});
+
+gulp.task('build', gulp.series('babelTest'));
+
+
   
